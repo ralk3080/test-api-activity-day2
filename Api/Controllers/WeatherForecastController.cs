1c26a9b93ece352a99f7c6f0c60c4c9b5190312c
Raul Diaz <ralk3080@gmail.com>
2026-03-10 14:17:43 -0600

    cambio a controller

--- Api/Controllers/WeatherForecastController.cs
+++ Api/Controllers/WeatherForecastController.cs
@@ -14,6 +14,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        //hola
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
